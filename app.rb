58e11efbdfb6123e1568b18e9677d0c0703f62fc
Brandon Green <brandon@brandonleeproductions.com>
2018-09-04 00:53:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Lynie!"
   end
-  get '/hometown' do
-    "My hometown is Buffalo!"
-  end
-  get '/favorite-song' do
-    "My favorite song is any by U2!"
-  end
+  # get '/hometown' do
+  #   "My hometown is Buffalo!"
+  # end
+  # get '/favorite-song' do
+  #   "My favorite song is any by U2!"
+  # end
 end
